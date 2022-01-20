3b22303b83f9ca832fb49f3867787bdea67271af
gandharv <gandharvkumargarg@gmail.com>
2022-01-20 17:19:51 +0530

    add test for slack

--- plugins/packages/slack/__tests__/slack.test.js
+++ plugins/packages/slack/__tests__/slack.test.js
@@ -1,7 +1,24 @@
 'use strict';
 
 const slack = require('../lib');
+const nock = require('nock');
 
 describe('slack', () => {
-    it.todo('needs tests');
+    const _slack = new slack.default()
+    describe('slack operations', () => {
+      const sourceOptions = { access_token: '123456' };
+      test('#list_users', async () => {
+        nock(`https://slack.com`).get('/api/users.list').reply(200, { body: "{}", message: 'ok' });
+        const queryOptions = { operation: 'list_users' }
+        const response = await _slack.run(sourceOptions, queryOptions);
+        expect(response.status).toEqual('ok');
+		  });
+
+      test('#send_message', async () => {
+        nock(`https://slack.com`).post('/api/chat.postMessage').reply(200, { body: "{}", message: 'ok' });
+        const queryOptions = { operation: 'send_message', channel: 1, message: 'hello', sendAsUser: false }
+        const response = await _slack.run(sourceOptions, queryOptions);
+        expect(response.status).toEqual('ok');
+		  });
+    });
 });
