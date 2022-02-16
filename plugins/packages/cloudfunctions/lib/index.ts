bbc213a4aae565cad4be9b516d0420c6a34e8770
stepinfwd <stepinfwd@gmail.com>
2022-02-17 02:05:30 +0530

    trigger update

--- plugins/packages/cloudfunctions/lib/index.ts
+++ plugins/packages/cloudfunctions/lib/index.ts
@@ -1,23 +1,63 @@
-import { QueryError, QueryResult, QueryService, ConnectionTestResult } from '@tooljet-plugins/common';
-import { SourceOptions, QueryOptions } from './types';
-const {CloudFunctionsServiceClient} = require('@google-cloud/functions');
-const client = new CloudFunctionsServiceClient();
+import { QueryError, QueryResult, QueryService } from "@tooljet-plugins/common";
+import { SourceOptions, QueryOptions } from "./types";
+import got, { Headers } from "got";
 
 export default class Cloudfunctions implements QueryService {
-  async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
+  authHeader(token: string): Headers {
     return {
-      status: 'ok',
-      data: {},
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
     };
   }
 
-  async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
-    // const client: Storage = await this.getConnection(sourceOptions);
+  async run(
+    sourceOptions: SourceOptions,
+    queryOptions: QueryOptions
+  ): Promise<QueryResult> {
+    let result = {};
+    let response = null;
+    const operation = queryOptions.operation;
+
+    const apiKey = sourceOptions.api_key;
+
+    try {
+      switch (operation) {
+        case "http_triggers": {
+          response = await got(
+            `https://asia-south1-tooljet-test-338806.cloudfunctions.net/stepin`,
+            {
+              method: "post",
+              headers: this.authHeader(apiKey),
+            }
+          );
+
+          result = JSON.parse(response.body);
+          console.log("response-------->", result);
+          break;
+        }
+
+        case "pubsub_triggers": {
+          // const recordId = queryOptions.record_id;
+
+          response = await got(
+            `https://asia-south1-tooljet-test-338806.cloudfunctions.net/stepin`,
+            {
+              headers: this.authHeader(apiKey),
+            }
+          );
+
+          result = JSON.parse(response.body);
+          break;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      throw new QueryError("Query could not be completed", error.message, {});
+    }
 
     return {
-      status: 'ok',
+      status: "ok",
+      data: result,
     };
   }
-
-
 }
--- plugins/packages/cloudfunctions/lib/types.ts
+++ plugins/packages/cloudfunctions/lib/types.ts
@@ -1,4 +1,4 @@
-export type SourceOptions = {};
+export type SourceOptions = { api_key: string; };
 export type QueryOptions = {
   operation: string;
 };
