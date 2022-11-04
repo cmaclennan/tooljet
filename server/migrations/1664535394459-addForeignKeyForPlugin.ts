d26a66269f7059d9ee55db7811da3b7030ae20af
gandharv <gandharvkumargarg@gmail.com>
2022-11-04 13:44:49 +0530

    fix: drop foreign key constraint

--- server/migrations/1664535394459-addForeignKeyForPlugin.ts
+++ server/migrations/1664535394459-addForeignKeyForPlugin.ts
@@ -2,6 +2,10 @@ import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
 
 export class addForeignKeyForPlugin1664535394459 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    const dsTable = await queryRunner.getTable('data_sources');
+    const dsForeignKey = dsTable.foreignKeys.find((fk) => fk.columnNames.indexOf('plugin_id') !== -1);
+    await queryRunner.dropForeignKey('data_sources', dsForeignKey);
+
     await queryRunner.createForeignKey(
       'data_sources',
       new TableForeignKey({
@@ -11,6 +15,10 @@ export class addForeignKeyForPlugin1664535394459 implements MigrationInterface {
         onDelete: 'CASCADE',
       })
     );
+
+    const dqTable = await queryRunner.getTable('data_queries');
+    const daForeignKey = dqTable.foreignKeys.find((fk) => fk.columnNames.indexOf('plugin_id') !== -1);
+    await queryRunner.dropForeignKey('data_queries', daForeignKey);
 
     await queryRunner.createForeignKey(
       'data_queries',
